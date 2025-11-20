234277ed145db8c5c0a6a2134dde67c66e4e0e62
unknown <sheikhalnahian@gmail.com>
2025-11-20 13:07:39 +0600

    login page is added

---
+++ src/Layouts/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from "../../Components/Logo/Logo";
+import { Outlet } from 'react-router';
+import authImage from '../.../../../assets/authImage.png'
+const AuthLayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto my-5'>
+            <Logo></Logo>
+            <div className='flex items-center'>
+                <div className='flex-1'>
+                <Outlet></Outlet>
+                </div>
+                <div className='flex-1'>
+                    <img src={authImage} alt="Authenication" />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default AuthLayout;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 import RootLayouts from "../Layouts/RootLayouts/RootLayouts";
 import Home from "../Pages/Home/Home/Home";
 import Coverage from "../Pages/Coverage/Coverage";
+import AuthLayout from "../Layouts/AuthLayout/AuthLayout";
+import Login from "../Pages/Auth/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -19,6 +21,21 @@ const router = createBrowserRouter([
             }
         ]
 
+    },
+    {
+        path: '/',
+        Component: AuthLayout,
+        children: [
+            {
+                path: '/login',
+                Component: Login
+
+            },
+            {
+                path: '/register'
+
+            }
+        ]
     }
 ])
 export default router;
